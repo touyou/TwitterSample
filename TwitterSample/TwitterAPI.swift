8ede5cb4ea84c0a00ec00030958b0716e53cdb07
Fujii Yosuke <fujiyou1121@gmail.com>
2016-02-14 09:50:33 +0900

    extended TwitterAPI Class

--- TwitterSample/TwitterAPI.swift
+++ TwitterSample/TwitterAPI.swift
@@ -19,10 +19,23 @@ class TwitterAPI {
     }
     
     // Swift2でエラーが出ないようにいじった
-    class func getHomeTimeLine(tweets: [TWTRTweet]->(), error: (NSError) -> ()) {
+    // 普通のタイムライン
+    class func getHomeTimeLine(tweets: [TWTRTweet]->(), error: (NSError)->()) {
+        getAnyStatus(tweets, error: error, path: "/statuses/home_timeline.json")
+    }
+    // 自分に対するメンションのタイムライン
+    class func getMentionTimeLine(tweets: [TWTRTweet]->(), error: (NSError)->()) {
+        getAnyStatus(tweets, error: error, path: "/statuses/mention_timeline.json")
+    }
+    // 自分のリツイートされた通知
+    class func getRetweetTimeLine(tweets: [TWTRTweet]->(), error: (NSError)->()) {
+        getAnyStatus(tweets, error: error, path: "statuses/retweets_of_me.json")
+    }
+    
+    // TimeLine一般を取得する
+    class func getAnyStatus(tweets: [TWTRTweet]->(), error: (NSError)->(), path: String) {
         let api = TwitterAPI()
         var clientError: NSError?
-        let path = "/statuses/home_timeline.json"
         let endpoint = api.baseURL + api.version + path
         let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("GET", URL: endpoint, parameters: nil, error: &clientError)
         
@@ -41,5 +54,30 @@ class TwitterAPI {
                 error(err!)
             }
         })
+
     }
+    // POSTアクションはこっち
+    class func postAnyStatus(tweets: [TWTRTweet]->(), error: (NSError)->(), path: String) {
+        let api = TwitterAPI()
+        var clientError: NSError?
+        let endpoint = api.baseURL + api.version + path
+        let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("POST", URL: endpoint, parameters: nil, error: &clientError)
+        Twitter.sharedInstance().APIClient.sendTwitterRequest(request, completion: {
+            response, data, err in
+            if err == nil {
+                do {
+                    let json: AnyObject? = try NSJSONSerialization.JSONObjectWithData(data!,
+                        options: NSJSONReadingOptions.MutableContainers)
+                    if let jsonArray = json as? NSArray {
+                        tweets(TWTRTweet.tweetsWithJSONArray(jsonArray as [AnyObject]) as! [TWTRTweet])
+                    }
+                } catch {
+                }
+            } else {
+                error(err!)
+            }
+        })
+
+    }
+    
 }
---
+++ TwitterSample/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TwitterSample
+//
+//  Created by 藤井陽介 on 2016/02/13.
+//  Copyright © 2016年 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
